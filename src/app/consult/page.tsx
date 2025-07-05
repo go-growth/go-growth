91872fccadd7d9fe4eb11918e17750838c9bc057
Armaan <armaanpal25@gmail.com>
2025-07-06 02:36:28 +0530

    new file and changes

---
+++ src/app/consult/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GoGrowthConsult from '@/components/GoGrowthConsult'
+
+const page = () => {
+  return (
+    <div><GoGrowthConsult /></div>
+  )
+}
+
+export default page
