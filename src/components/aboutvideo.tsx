63ef70696e3af6dd37cb2cd67042d145c12d4dfa
go-growth <gogrowth47@gmail.com>
2025-10-28 19:03:00 +0530

    Update aboutvideo.tsx

--- src/components/aboutvideo.tsx
+++ src/components/aboutvideo.tsx
@@ -24,9 +24,9 @@ export default function Aboutvideo() {
     return (
         <div className="min-h-[60vh] md:min-h-[90vh] flex items-center justify-center w-full p-4 pb-6 md:pb-20">
             <div className="max-w-6xl w-full">
-                <h2 className="max-w-7xl mx-auto text-center text-4xl font-semibold mb-16">Our Founder</h2>
+                <h2 className="max-w-7xl mx-auto text-center text-4xl font-semibold mb-6 md:mb-16">Our Founder</h2>
 
-                <div className="flex justify-center mb-16">
+                <div className="flex justify-center mb-8 md:mb-16">
                     <div className="relative w-full max-w-7xl aspect-video border border-zinc-500 rounded-lg">
                         <div className="w-full h-full rounded-lg overflow-hidden">
                             <video
